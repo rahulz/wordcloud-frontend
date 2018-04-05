bfab71ad32cb55f78f19d6284e1cb04d576af08f
Rahul <rahul.k@cubettech.com>
2018-04-05 10:24:35 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  input = "";
+  image = "";
+
+  constructor(private http: HttpClient) {
+  }
+
+  gen() {
+    this.http.post("http://localhost:5000/get_cloud/png", {text: this.input})
+      .subscribe(res => {
+        this.image = res.img;
+
+        }
+      );
+
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { SafeUrlPipe } from './safe-url';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, SafeUrlPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
